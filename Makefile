fee5a68ce1ed364502792e135f4274d70a242a52
Janos Miko <mixe3y@gmail.com>
2023-05-05 09:16:49 +0200

    version update

---
+++ Makefile
@@ -0,0 +1,26 @@
+.DEFAULT_GOAL 	= help
+
+SHELL         	= bash
+project       	= reward
+GIT_AUTHOR    	= janosmiko
+
+help: ## Outputs this help screen
+	@grep -E '(^[\/a-zA-Z0-9_-]+:.*?##.*$$)|(^##)' $(MAKEFILE_LIST) | awk 'BEGIN {FS = ":.*?## "}{printf "\033[32m%-30s\033[0m %s\n", $$1, $$2}' | sed -e 's/\[32m##/[33m/'
+
+# If the first argument is "gen"...
+ifeq (gen,$(firstword $(MAKECMDGOALS)))
+  # use the rest as arguments for "run"
+  GEN_ARGS := $(wordlist 2,$(words $(MAKECMDGOALS)),$(MAKECMDGOALS))
+  # ...and turn them into do-nothing targets
+  $(eval $(GEN_ARGS):;@:)
+endif
+
+## —— Commands —————————————————————————————————————————————————————————
+.PHONY: remove-jsonld
+remove-jsonld: ## Remove JSON-LD files
+	python3 hack/main.py --input api.yaml --output reward-api.yaml
+
+.PHONY: generate
+generate: ## Generate Go code from OpenAPI spec
+	docker run --rm -v "$${PWD}:/local" openapitools/openapi-generator-cli:v6.5.0 generate -i /local/reward-api.yaml -c /local/config.yaml -g go -o /local/rewardcloud
+	rm -f rewardcloud/go.mod && rm -f rewardcloud/go.sum
---
+++ hack/main.py
@@ -0,0 +1,42 @@
+import sys
+import argparse
+from pathlib import Path
+import ruamel.yaml
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-i", "--input", help="input file in yaml format", required=True, type=argparse.FileType('r'))
+parser.add_argument("-o", "--output", help="output file in yaml format, defaults to output.yaml", default="reward-api.yaml", type=argparse.FileType('w'))
+
+args = parser.parse_args()
+
+in_file = args.input
+out_file = args.output
+
+yaml = ruamel.yaml.YAML()
+yaml.indent(mapping=2, sequence=4, offset=2)
+yaml.preserve_quotes = True
+data = yaml.load(in_file)
+
+delete = []
+
+
+def rm_merge(d, s):
+    if isinstance(d, dict):
+        if s in d:
+            del d[s]
+        for k in list(d.keys()):
+            if str(k).find(s) != -1:
+                del d[k]
+            else:
+                rm_merge(d[k], s)
+    elif isinstance(d, list):
+        for item in d:
+            rm_merge(item, s)
+
+
+rm_merge(data, 'application/ld+json')
+rm_merge(data, 'application/merge-patch+json')
+rm_merge(data, 'application/hal+json')
+rm_merge(data, '.jsonld')
+
+yaml.dump(data, out_file)
